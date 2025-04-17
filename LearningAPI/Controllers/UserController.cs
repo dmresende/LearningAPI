3dfc349a73e9e1b49b2c3bfe3731cd3e1f7ec331
Douglas Resende <Douglas Resende@DESKTOP-OV2STEO>
2025-04-17 18:23:30 -0300

    First getbyid route with provisional documentation

---
+++ LearningAPI/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LearningAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    [Route("{id}")]
+    [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+    public IActionResult GetById([FromRoute] int id)
+    {
+        var respoonse = new User
+        {
+            Id= id,
+            Age = 25,
+            Name = "John Doe"
+        };
+
+        return Ok(respoonse);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+    public IActionResult PostUser()
+    {
+        return Created();
+    }
+}
---
+++ LearningAPI/User.cs
@@ -0,0 +1,8 @@
+﻿namespace LearningAPI;
+
+public class User
+{
+    public int Id { get; set; }
+    public int Age { get; set; }
+    public string Name{ get; set; } = string.Empty;
+}
